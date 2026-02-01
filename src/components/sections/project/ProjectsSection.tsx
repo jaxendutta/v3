c7759cc21a5ef7a5035f1c04cbf0476a8c33602b
Jaxen Dutta <jaxendutta@gmail.com>
2026-01-31 21:37:31 -0500

    fix: add chain prop to ProjectCard in ProjectsSection for improved functionality

--- src/components/sections/project/ProjectsSection.tsx
+++ src/components/sections/project/ProjectsSection.tsx
@@ -34,6 +34,7 @@ export default function ProjectsSection() {
                                 id={id}
                                 project={projectsData[id]}
                                 reversed={index % 2 !== 0}
+                                chain
                             />
                         </div>
                     ))}
